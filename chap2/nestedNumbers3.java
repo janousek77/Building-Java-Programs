a7a29ec8380992757582c6c027a9e56adb749773
Chris Janousek <janousek77@gmail.com>
2018-08-30 16:14:27 -0700

    added #14

---
+++ chap2/nestedNumbers3.java
@@ -0,0 +1,14 @@
+// Exercise #14
+
+class nestedNumbers3{
+  public static void main(String[] args) {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 9; j>=0; j--) {
+        for (int k = 0; k<j; k++) {
+          System.out.print(j);
+        }
+      }
+      System.out.println();
+    }
+  }
+}
