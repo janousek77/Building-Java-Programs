35e7bd9c28fbbc0270da658cfc89f4030778cd24
Chris Janousek <janousek77@gmail.com>
2018-08-30 16:27:13 -0700

    fifteen done

---
+++ chap2/printDesign.java
@@ -0,0 +1,18 @@
+// Exercise #15
+
+class printDesign{
+  public static void main(String[] args) {
+    for (int i = 5; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("-");
+      }
+      for (int j = 1; j <= (11 - 2 * i); j++) {
+        System.out.print(11 - 2 * i);
+      }
+      for (int j = 1; j <= i; j++) {
+        System.out.print("-");
+      }
+      System.out.println();
+    }
+  }
+}
