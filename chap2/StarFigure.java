e6ebaa0a3e2017e7937a4383c8d483006d9c7356
Chris Janousek <janousek77@gmail.com>
2018-08-31 12:11:51 -0700

    all problems finished for chap 2

--- chap2/StarFigure.java
+++ chap2/StarFigure.java
@@ -2,14 +2,14 @@
 
 public class StarFigure{
   public static void main(String[] args) {
-    for (int i = 4; i >= 0; i--) {
-      for (int j = i; j > 0; j--) {
+    for (int i = 5; i > 0; i--) {
+      for (int j = i; j > 1; j--) {
         System.out.print("////");
       }
-      for (int j = 1; j < (32 - (i*8-1)); j++) {
-        System.out.print("*");
+      for (int j = i; j < 5; j++) {
+        System.out.print("********");
       }
-      for (int j = i; j > 0; j--) {
+      for (int j = i; j > 1; j--) {
         System.out.print("\\\\\\\\");
       }
       System.out.println();
--- chap2/StarFigure2.java
+++ chap2/StarFigure2.java
@@ -1,16 +1,16 @@
 // Exercise #21
 
 public class StarFigure2{
-  public static final int ROWS = 3;
+  public static final int ROWS = 5;
   public static void main(String[] args) {
     for (int i = ROWS; i > 0; i--) {
-      for (int j = i; j > 0; j--) {
+      for (int j = i; j > 1; j--) {
         System.out.print("////");
       }
-      for (int j = 1; j < (ROWS - (i*8-1)); j++) {
-        System.out.print("*");
+      for (int j = i; j < ROWS; j++) {
+        System.out.print("********");
       }
-      for (int j = i; j > 0; j--) {
+      for (int j = i; j > 1; j--) {
         System.out.print("\\\\\\\\");
       }
       System.out.println();
