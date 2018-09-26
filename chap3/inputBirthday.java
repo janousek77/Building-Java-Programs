3aac0d41498e0e248739671e591d2df3b8b1a121
Chris Janousek <janousek77@gmail.com>
2018-09-26 14:30:02 -0700

    21 done

---
+++ chap3/inputBirthday.java
@@ -0,0 +1,20 @@
+// Exercise 20
+
+import java.util.*;
+
+class inputBirthday{
+  public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+    inputBirthday(console);
+  }
+
+  public static void inputBirthday(Scanner console) {
+    System.out.print("On what day of the month were you born? ");
+    int day = console.nextInt();
+    System.out.print("What is the name of the month in which you were born? ");
+    String month = console.next();
+    System.out.print("During what year were you born? ");
+    int year = console.nextInt();
+    System.out.println("You were born on " + month + " " + day + ", "+year + ". You're mighty old!");
+  }
+}
