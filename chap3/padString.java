a2d4a2973390a5bb12da9cea565c1b30915e2bb5
Chris Janousek <janousek77@gmail.com>
2018-09-26 13:54:14 -0700

    finished 17

---
+++ chap3/padString.java
@@ -0,0 +1,14 @@
+// Exercise 17
+
+class padString{
+  public static void main(String[] args) {
+    System.out.println(padString("hello", 8));
+  }
+
+  public static String padString(String word, int length) {
+    for (int i = 0; i<length-word.length(); ) {
+      word=" "+word;
+    }
+    return word;
+  }
+}
